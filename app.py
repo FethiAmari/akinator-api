e4b0ecf8f9f85546958be77ff392614aff521af5
Fethi Amari <fethi.amari06@gmail.com>
2025-04-15 10:35:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from akinator_python import Akinator
+from myaki_api import Akinator
 import uuid
 
 app = Flask(__name__)
